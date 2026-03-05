011e33ca91d09d8cbb82b59b21513dfcfa7e0199
Naut van der Winden <nwinden@proton.me>
2026-03-05 13:45:43 +0000

    FIXED: Last commit tracker Should finally work. It escaped my attention that `cookieStore` is only supported by chrome and edge. It now uses `document.cookie` and string comprehension

--- script/fetch_commit.js
+++ script/fetch_commit.js
@@ -15,13 +15,9 @@ function updateCommitTracker(json){
 	dt = json["commit"]["author"]["date"];
 	msg = json["commit"]["message"];
 	const ed = new Date();
-	ed.setTime(ed.getTime() + (1000 * 10));
-
-	cookieStore.set({
-		name:"gh-commit",
-		value:`${dt}${msg}`,
-		expires: Date.now() + (1000 * 10)
-	})
+	ed.setTime(ed.getTime() + (1000 * 10 * 60)); //Expire 10 minutes from now
+	cookieString = encodeURI(`gh-commit=${dt}${msg};`) + `expires=${ed.toUTCString()}`;
+	document.cookie = cookieString;
 	COMMIT_DT_OBJ.innerHTML = dt;
 	COMMIT_MSG_OBJ.innerHTML = msg;
 } 
@@ -37,11 +33,19 @@ function updateCommitTrackerFromCache(res){
 }
 
 //Check cookie for last refresh time
-cookie = document.cookie;
-cookieStore.get("gh-commit").then(res=>{
-	if(res == null){
-		getLatestCommit();
-	} else {
-		updateCommitTrackerFromCache(res);
+cookies = document.cookie.split("; ");
+
+foundCookie = false;
+cookies.forEach((cookie)=>{
+	console.log(cookie);
+	//There has already been an API call in the last 10 minutes, use the cached value to avoid ratelimit
+	if (cookie.startsWith("gh-commit=")){
+		val = cookie.slice(10);
+		updateCommitTrackerFromCache(val);
+		foundCookie = true;
 	}
-})
+});
+//If the cookie doesn't exist, use the api
+if (!foundCookie){
+	getLatestCommit();
+}
